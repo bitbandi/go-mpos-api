00334754b33ae59a54fae64d5fe748357d9c682f
elbandi <bandispam@gmail.com>
2016-09-17 20:20:25 +0200

    Handle sharerate string type

--- userstatus.go
+++ userstatus.go
@@ -1,10 +1,62 @@
 package mpos
 
+import (
+	"strconv"
+	"encoding/json"
+)
+
+type strFloat64 float64
+
+func (s *strFloat64) UnmarshalJSON(data []byte) error {
+	var v float64
+	var err error
+	if len(data) == 0 {
+		*s = 0
+		return nil
+	}
+
+	if data[0] != '"' || data[len(data) - 1] != '"' {
+		v, err = strconv.ParseFloat(string(data), 64)
+	} else {
+		v, err = strconv.ParseFloat(string(data[1:len(data) - 1]), 64)
+	}
+	if err != nil {
+		return err
+	}
+
+	*s = strFloat64(v)
+	return nil
+}
+
+func (s *strFloat64) Float64() float64 {
+	return float64(*s)
+}
+
 type UserStatus struct {
 	Username  string     `json:"username"`
 	Shares    UserShares `json:"shares"`
 	Hashrate  float64        `json:"hashrate"`
 	Sharerate float64        `json:"sharerate"`
+}
+
+// Used to avoid recursion in UnmarshalJSON below.
+type userStatus struct {
+	Username  string     `json:"username"`
+	Shares    UserShares `json:"shares"`
+	Hashrate  float64        `json:"hashrate"`
+	Sharerate strFloat64        `json:"sharerate"`
+}
+
+func (s *UserStatus) UnmarshalJSON(data []byte) error {
+	var us userStatus
+	if err := json.Unmarshal(data, &us); err != nil {
+		return err
+	}
+	s.Username = us.Username
+	s.Shares = us.Shares
+	s.Hashrate = us.Hashrate
+	s.Sharerate = float64(us.Sharerate)
+	return nil
 }
 
 type UserShares struct {
